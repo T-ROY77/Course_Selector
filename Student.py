f3b859bf3cf14dc0b11336c880b850099a754e21
Elmar Imanov <elmar.imanov@sjsu.edu>
2023-05-16 23:01:24 -0700

    student changes

--- Student.py
+++ Student.py
@@ -1,35 +1,21 @@
 class Student:
-    def __init__(self, name, major, GPA, availability, prevCourses):
+    def __init__(self, name, age, major, GPA, school_year, availability, prevCourses):
         self.name = name
+        self.age = age
         self.major = major
         self.GPA = GPA
+        self.school_year = school_year
         self.availability = availability
         self.prevCourses = prevCourses
         self.courseList = []
 
-    def getName(self):
-        return self.name
-
-    def getMajor(self):
-        return self.major
-
-    def getGPA(self):
-        return self.GPA
-
-    def getAvailability(self):
-        return self.availability
-
-    def getprevCourses(self):
-        return self.prevCourses
-
-    def showAvailablitySchedule(self):
-        print("-------------------------")
-        print("   Mon/Wed      Tues/Thurs")
-        print("    %s - %s         %s - %s" %(self.availability[0][0], self.availability[0][1], self.availability[1][0], self.availability[1][1]))
-        print("-------------------------")
-
-    def showTranscript(self):
-        print("-------------------------")
-        for x in self.prevCourses:
-            print("CS%s" %x)
-        print("-------------------------")
+print("Please enter the following information:")
+print("----------------------------------------")
+name = input("  Name: ")
+age = input("  Age: ")
+major = input("  Major: ")
+gpa = input("  GPA: ")
+school_year = input("  School Year: ")
+availability = input("  Availability: ")
+prevCourses = input(" previous courses: ")
+courseList = input("Course list: ")
